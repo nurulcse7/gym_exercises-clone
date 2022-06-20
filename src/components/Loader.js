8d337cf3144bd461bea44d88c80b611571df305b
Nurul Islam <nurulbdpersonal@gmail.com>
2022-06-20 23:12:08 +0600

    final code

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+// import { InfinitySpin } from 'react-loader-spinner';
+
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    
+  </Stack>
+);
+
+export default Loader;
+
+// https://www.npmjs.com/package/react-loader-spinner // npm install react-loader-spinner --save 
+
+// <InfinitySpin color="grey" /> // Not available
+
+// TailSpin	<TailSpin color="#00BFFF" height={80} width={80} />  // available
